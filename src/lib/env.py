7ff13c7aa1b09673256e94c0325d1817a351b30d
MehmetYukselSekeroglu <dijital_evren@protonmail.com>
2025-02-08 15:30:28 +0300

    Add custom ports

--- src/lib/env.py
+++ src/lib/env.py
@@ -1,9 +1,24 @@
 
 
 
-DEFAULT_PORTS = [80,443,8080,8443, 37777]
+DEFAULT_PORTS = [80,  # HTTP
+                 443,  # HTTPS
+                 8080, # HTTP
+                 8443, # HTTPS
+                 37777, # Dahua
+                 34567, # Hikvision
+                 9001, # Axis
+                 9002, # Axis
+                 8899, # Mobotix
+                 8899, # Vivotek
+                 8000, # Avigilon
+                 7001, # Arecont
+                 8091, # Panasonic
+                 8999, # Sony
+                 8086  # Geovision
+                ]
 URL_PATHS = [
-    "/doc/page/login.asp",
-    "/",
+    "/doc/page/login.asp", # Hikvision
+    "/", # Generic 
 ]
 SUB_THREAD_COUNT = 10
